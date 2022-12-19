cde7fb11753f2a5783fd2f9d2afee684ef424f5c
yekkalurisurendra <116298046+yekkalurisurendra@users.noreply.github.com>
2022-12-19 15:19:26 +0530

    1234

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
   access_key = "AKIAWJX5GMXTAUXH5NRW"
   secret_key = "+xGOAk6J+eROv3lMqKGWg0POeYfpMQ1EVJ486SVr"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0cca134ec43cf708f"
+  ami = "ami-0b5eea76982371e91"
   instance_type = var.instance_type
   tags= {
     Name = "suri"
